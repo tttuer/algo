423591ccd0d2e003f33108d23590bc53bcee0dd1
tttuer <tttuer1@naver.com>
2026-04-23 15:24:32 +0900

    1201C

--- codeforce_p/1201C.py
+++ codeforce_p/1201C.py
@@ -9,21 +9,22 @@ a.sort()
 
 mid = n//2
 
-a_diff = []
+left = a[mid]
+right = a[mid]+k
+answer = left
 
-for i in range(n):
-    a_diff.append(a[i]-a[mid])
+while left <= right:
+    m = (left+right)//2
 
-prefix_sum = [0]*n
-
-for i in range(mid+1,n):
-    prefix_sum[i] = prefix_sum[i-1]+a_diff[i]
-
-end = bisect_right(prefix_sum, k)
-answer = a[mid]
-
-answer = a[end-1]
-k -= prefix_sum[end-1]
-answer += k//(end-mid)
+    cost = 0
+    for i in range(mid, n):
+        if a[i] < m:
+            cost += m-a[i]
+    
+    if cost <= k:
+        answer = m
+        left = m+1
+    else:
+        right = m-1
 
 print(answer)
