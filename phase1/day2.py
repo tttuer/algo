7feae04854545cd4420f6ee7d88726d1906eac17
tttuer <tttuer1@naver.com>
2026-05-11 09:35:34 +0900

    phase1 day2

---
+++ phase1/day2.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+import heapq
+q = []
+heapq.heappush(q, a)
+heapq.heappop(q)
+
+arr = [[0]*m for _ in range(n)]
+
+if all(v for v in arr):
+if any(v%2==0 for v in arr):
+
+a,b = b,a
+arr[i],arr[j] = arr[j],arr[i]
+
+from collections import Counter
+cnt = Counter(arr)
+
+from collections import defaultdict
+d = defaultdict(list)
+d[0].append(0)
+
+new = arr[:]
+new = list(arr)
+
